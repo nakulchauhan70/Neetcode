0602ec84d8a9fcfbe068a0c1e77869a2c1dba489
Nakul Chauhan <nakulchauhan@cppib.com>
2022-07-20 18:16:49 +0530

    Initial commit

---
+++ src/com/code150/ContainsDuplicate.java
@@ -0,0 +1,22 @@
+package com.code150;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        System.out.println(containsDuplicate(new int[]{1, 2}));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
